6d6c030250e6d5b53f50ce8da7bb43967d0f477e
Brandon Donnelson <branflake2267@gmail.com>
2013-03-03 18:40:23 -0800

    GXT grid works, loads data initially.

--- Directory/src/org/gonevertical/pm/directory/client/application/widgets/archetype/list/ArchetypeListView.java
+++ Directory/src/org/gonevertical/pm/directory/client/application/widgets/archetype/list/ArchetypeListView.java
@@ -3,20 +3,19 @@ package org.gonevertical.pm.directory.client.application.widgets.archetype.list;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.gonevertical.pm.directory.client.events.archetypes.ArchetypeObserver;
 import org.gonevertical.pm.directory.client.rest.jso.ArchetypeJso;
-import org.gonevertical.pm.directory.client.rest.util.RestList;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
-import com.sencha.gxt.core.client.IdentityValueProvider;
 import com.sencha.gxt.data.client.loader.RpcProxy;
 import com.sencha.gxt.data.shared.ListStore;
 import com.sencha.gxt.data.shared.ModelKeyProvider;
@@ -27,11 +26,11 @@ import com.sencha.gxt.data.shared.loader.PagingLoader;
 import com.sencha.gxt.widget.core.client.FramedPanel;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer.VerticalLayoutData;
-import com.sencha.gxt.widget.core.client.event.RefreshEvent;
-import com.sencha.gxt.widget.core.client.grid.CheckBoxSelectionModel;
 import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
 import com.sencha.gxt.widget.core.client.grid.ColumnModel;
 import com.sencha.gxt.widget.core.client.grid.Grid;
+import com.sencha.gxt.widget.core.client.selection.SelectionChangedEvent;
+import com.sencha.gxt.widget.core.client.selection.SelectionChangedEvent.SelectionChangedHandler;
 import com.sencha.gxt.widget.core.client.toolbar.PagingToolBar;
 
 public class ArchetypeListView extends ViewWithUiHandlers<ArchtypeListUiHandlers> implements
@@ -43,7 +42,7 @@ public class ArchetypeListView extends ViewWithUiHandlers<ArchtypeListUiHandlers
 
   @UiField
   FlowPanel tableContanier;
-  
+
   private boolean intialized = false;
 
   private ArchetypeColumnProperties columnProperties;
@@ -51,7 +50,7 @@ public class ArchetypeListView extends ViewWithUiHandlers<ArchtypeListUiHandlers
   @Inject
   public ArchetypeListView(final Binder binder, ArchetypeColumnProperties columnProperties) {
     this.columnProperties = columnProperties;
-    
+
     initWidget(binder.createAndBindUi(this));
   }
 
@@ -65,7 +64,11 @@ public class ArchetypeListView extends ViewWithUiHandlers<ArchtypeListUiHandlers
     initTable();
   }
 
+  /**
+   * TODO selected event updates display
+   */
   private void initTable() {
+    // Data Provider
     RpcProxy<PagingLoadConfig, PagingLoadResult<ArchetypeJso>> proxy = new RpcProxy<PagingLoadConfig, PagingLoadResult<ArchetypeJso>>() {
       @Override
       public void load(PagingLoadConfig loadConfig, AsyncCallback<PagingLoadResult<ArchetypeJso>> callback) {
@@ -73,6 +76,7 @@ public class ArchetypeListView extends ViewWithUiHandlers<ArchtypeListUiHandlers
       }
     };
 
+    // List Store
     ListStore<ArchetypeJso> store = new ListStore<ArchetypeJso>(new ModelKeyProvider<ArchetypeJso>() {
       @Override
       public String getKey(ArchetypeJso item) {
@@ -80,35 +84,29 @@ public class ArchetypeListView extends ViewWithUiHandlers<ArchtypeListUiHandlers
       }
     });
 
-    final PagingLoader<PagingLoadConfig, PagingLoadResult<ArchetypeJso>> loader = new PagingLoader<PagingLoadConfig, PagingLoadResult<ArchetypeJso>>(proxy);
+    // Paging Loader
+    final PagingLoader<PagingLoadConfig, PagingLoadResult<ArchetypeJso>> loader = new PagingLoader<PagingLoadConfig, PagingLoadResult<ArchetypeJso>>(
+        proxy);
     loader.setRemoteSort(true);
-    loader.addLoadHandler(new LoadResultListStoreBinding<PagingLoadConfig, ArchetypeJso, PagingLoadResult<ArchetypeJso>>(store));
+    loader.addLoadHandler(new LoadResultListStoreBinding<PagingLoadConfig, ArchetypeJso, PagingLoadResult<ArchetypeJso>>(
+            store));
 
+    // Toolbar
     final PagingToolBar toolBar = new PagingToolBar(PAGE_SIZE);
     toolBar.getElement().getStyle().setProperty("borderBottom", "none");
     toolBar.bind(loader);
 
-    IdentityValueProvider<ArchetypeJso> identity = new IdentityValueProvider<ArchetypeJso>();
-    final CheckBoxSelectionModel<ArchetypeJso> sm = new CheckBoxSelectionModel<ArchetypeJso>(identity) {
-      @Override
-      protected void onRefresh(RefreshEvent event) {
-        // this code selects all rows when paging if the header checkbox is selected
-        if (isSelectAllChecked()) {
-          selectAll();
-        }
-        super.onRefresh(event);
-      }
-    };
+    // Name Column
+    ColumnConfig<ArchetypeJso, String> nameColumn = new ColumnConfig<ArchetypeJso, String>(columnProperties.name(),
+        100, "Name");
 
-    ColumnConfig<ArchetypeJso, String> nameColumn = new ColumnConfig<ArchetypeJso, String>(columnProperties.name(), 100, "Name");
-
-    List<ColumnConfig<ArchetypeJso, ?>> l = new ArrayList<ColumnConfig<ArchetypeJso, ?>>();
-    l.add(sm.getColumn());
-    l.add(nameColumn);
-
-    ColumnModel<ArchetypeJso> cm = new ColumnModel<ArchetypeJso>(l);
-
-    Grid<ArchetypeJso> grid = new Grid<ArchetypeJso>(store, cm) {
+    // Columns
+    List<ColumnConfig<ArchetypeJso, ?>> columnConfigList = new ArrayList<ColumnConfig<ArchetypeJso, ?>>();
+    columnConfigList.add(nameColumn);
+    ColumnModel<ArchetypeJso> columnModel = new ColumnModel<ArchetypeJso>(columnConfigList);
+    
+    // Grid
+    Grid<ArchetypeJso> grid = new Grid<ArchetypeJso>(store, columnModel) {
       @Override
       protected void onAfterFirstAttach() {
         super.onAfterFirstAttach();
@@ -120,17 +118,29 @@ public class ArchetypeListView extends ViewWithUiHandlers<ArchtypeListUiHandlers
         });
       }
     };
-    grid.setSelectionModel(sm);
+    
+    // Selection
+    grid.getSelectionModel().addSelectionChangedHandler(new SelectionChangedHandler<ArchetypeJso>() {
+      @Override
+      public void onSelectionChanged(SelectionChangedEvent<ArchetypeJso> event) {
+        getUiHandlers().gotoEdit(event.getSelection().get(0));
+      }
+    });
+    
+    // Grid properties
+    grid.setLoader(loader);
     grid.getView().setForceFit(true);
     grid.setLoadMask(true);
-    grid.setLoader(loader);
+    grid.setBorders(true);
+    grid.getView().setAutoExpandColumn(nameColumn);
 
+    // Frame
     FramedPanel framedPanel = new FramedPanel();
     framedPanel.setCollapsible(true);
-    framedPanel.setHeadingText("Paging Grid Example");
-    framedPanel.setPixelSize(500, 400);
-    framedPanel.addStyleName("margin-10");
+    framedPanel.setHeadingText("Archetypes");
+    framedPanel.setPixelSize(500, 427);
 
+    // Layout Container
     VerticalLayoutContainer vpLayoutCont = new VerticalLayoutContainer();
     vpLayoutCont.setBorders(true);
     vpLayoutCont.add(grid, new VerticalLayoutData(1, 1));
