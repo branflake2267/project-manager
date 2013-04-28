42b28fea01eee108679312101ca7e660335a357d
Brandon Donnelson <branflake2267@gmail.com>
2013-04-28 13:48:18 -0700

    Fixed relationships to unowned.

--- Directory/src/org/gonevertical/pm/directory/server/domain/Category.java
+++ Directory/src/org/gonevertical/pm/directory/server/domain/Category.java
@@ -11,7 +11,7 @@ import com.google.appengine.api.datastore.KeyFactory;
 
 @PersistenceCapable(detachable = "true")
 public class Category {
- 
+
   @Persistent
   private Key parent;
 
@@ -71,5 +71,29 @@ public class Category {
   public void setHasChildren(boolean hasChildren) {
     this.hasChildren = hasChildren;
   }
-  
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((key == null) ? 0 : key.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Category other = (Category) obj;
+    if (key == null) {
+      if (other.key != null)
+        return false;
+    } else if (!key.equals(other.key))
+      return false;
+    return true;
+  }
 }
