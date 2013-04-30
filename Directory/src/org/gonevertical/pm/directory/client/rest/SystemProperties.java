9b5b0a56ac831c601f44f9d2e787978fcb9365c8
Brandon Donnelson <branflake2267@gmail.com>
2013-04-29 22:48:32 -0700

    system properties

---
+++ Directory/src/org/gonevertical/pm/directory/client/rest/SystemProperties.java
@@ -0,0 +1,8 @@
+package org.gonevertical.pm.directory.client.rest;
+
+public class SystemProperties {
+  public final static String VERSION = "v1";
+  public String getVersion() {
+    return VERSION;
+  }
+}
