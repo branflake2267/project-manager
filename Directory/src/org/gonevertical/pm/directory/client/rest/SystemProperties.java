be1819c624da79c609af5c263ddc56882f495609
Brandon Donnelson <branflake2267@gmail.com>
2013-05-12 12:41:48 -0700

    Tweaked tag columns. Latest additions. Archetype collection added.

--- Directory/src/org/gonevertical/pm/directory/client/rest/SystemProperties.java
+++ Directory/src/org/gonevertical/pm/directory/client/rest/SystemProperties.java
@@ -1,8 +1,20 @@
 package org.gonevertical.pm.directory.client.rest;
 
 public class SystemProperties {
+  
   public final static String VERSION = "v1";
+  
+  /**
+   * Turn on the remote server for rest requests instead of locally.
+   */
+  public final static boolean DEBUG = true;
+  
   public String getVersion() {
     return VERSION;
   }
+  
+  public static boolean getDebugRemote() {
+    return DEBUG;
+  }
+  
 }
